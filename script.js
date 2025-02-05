e0fd278a1352c6edd872778ab63d5ed58b79fdac
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-05 13:58:03 -0600

    Basic Calculator Functionality Working
    Created basic functionality for arithmetic. Current issues
    are that text overflows, +/- and . don't work, and you can put
    multiple operators (of which only the last will be used).

--- script.js
+++ script.js
@@ -1,5 +1,79 @@
 let screen = document.getElementById("screen");
+// lhs rhs = left right hand side
 let sign, lhs, rhs, operator;
+
+function initialize() {
+    document.getElementById("screen").innerText = "";
+    console.log(`Current text is: ${document.getElementById("screen").innerText}`);
+    
+    sign = "+";
+    lhs = "";
+    rhs = "";
+    operator = "";
+}
+
+// ???????????? Maybe change to each number not whole expression
+// function changeSign() {
+//     if (sign === "+") {
+//         sign = "-"
+//         screen.innerText = sign + screen.innerText;
+//     } else {
+//         sign = "+"
+//         screen.innerText = screen.innerText.substring(1);
+//     }
+// }
+
+function numberPressed(numButton) {
+    numString = numButton.innerText;
+        screen.innerText = screen.innerText + numString;
+        rhs = rhs + numButton.innerText; 
+
+}
+
+function operatorPressed(operatorButton) {
+    screen.innerText = screen.innerText + operatorButton.innerText
+    if (operator === "") {
+        lhs = rhs;
+    } else if (operator !== "=") {
+        lhs = operate(lhs, rhs, operator);
+    }
+    rhs = "";
+
+    operator = operatorButton.innerText;
+    if (operator === "=") {
+        screen.innerText = lhs;
+    }
+}
+
+
+
+
+
+/*
+ *              UTILITY FUNCTIONS
+ */
+
+// Passed two numbers and and operator, computes.
+function operate(leftNum, rightNum, operator) {
+    let result;
+    leftNum = Number(leftNum);
+    rightNum = Number(rightNum);
+    switch (operator) {
+        case "+":
+            result = add(leftNum, rightNum);
+            break;
+        case "-":
+            result = subtract(leftNum, rightNum);
+            break;
+        case "*":
+            result = multiply(leftNum, rightNum);
+            break;
+        case "/":
+            result = divide(leftNum, rightNum);
+            break;
+        }
+    return result
+}
 
 function add(num1, num2) {
     return num1 + num2;
@@ -16,45 +90,3 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
-
-function operate(lhs, rhs, operator) {
-    switch (operand) {
-        case "+":
-            add(lhs, rhs);
-            break;
-        case "-":
-            add(lhs, rhs);
-            break;
-        case "*":
-            add(lhs, rhs);
-            break;
-        case "/":
-            add(lhs, rhs);
-            break;
-    }
-
-    /* Let left hand side be result and 
-    everything except sign be base vals */
-}
-
-function changeSign() {
-    if (sign === "+") {
-        sign = "-"
-        screen.innerText = sign + screen.innerText;
-    } else {
-        sign = "+"
-        screen.innerText = screen.innerText.substring(1);
-    }
-}
-
-function initialize() {
-    document.getElementById("screen").innerText = "0";
-    console.log(`Current text is: ${document.getElementById("screen").innerText}`);
-    
-    sign = "+";
-    lhs = 0;
-    rhs = 0;
-    operator = "";
-}
-
-// left and right hand side side
